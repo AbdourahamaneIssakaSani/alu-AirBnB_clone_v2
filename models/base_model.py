8d3b5c004d74b5896cb7e20bf80dc78ce042a294
Abissa <a.sani@alustudent.com>
2023-02-18 19:22:49 +0200

    6. DBStorage - fix missing primary key

--- models/base_model.py
+++ models/base_model.py
@@ -3,12 +3,17 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
 
 Base = declarative_base()
 
 
 class BaseModel:
     """A base class for all hbnb models"""
+    # used for db storage
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
         """Instantiates a new model"""
