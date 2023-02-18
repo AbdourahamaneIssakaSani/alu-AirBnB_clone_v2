dea48fedb1085838f4b54aa5aacdcbf09bca5c37
Abissa <a.sani@alustudent.com>
2023-02-18 19:55:12 +0200

    6. DBStorage - fix storage confusion

--- models/__init__.py
+++ models/__init__.py
@@ -10,12 +10,13 @@ from models.place import Place
 from models.review import Review
 
 if os.getenv("HBNB_TYPE_STORAGE") == "db":
-    from models.engine.file_storage import FileStorage
-
-    storage = FileStorage()
-    storage.reload()
-else:
     from models.engine.db_storage import DBStorage
 
     storage = DBStorage()
+    storage.reload()
+else:
+
+    from models.engine.file_storage import FileStorage
+
+    storage = FileStorage()
     storage.reload()
