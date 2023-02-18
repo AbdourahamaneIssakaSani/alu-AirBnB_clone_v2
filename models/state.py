67c36118c158b2b65377c2aa1b3a87e4a3a552b3
Abissa <a.sani@alustudent.com>
2023-02-18 19:08:32 +0200

    6. DBStorage - States and Cities

--- models/state.py
+++ models/state.py
@@ -9,10 +9,11 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = "states"
+    __tablename__ = 'states'
+
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state",
-                          cascade="all, delete-orphan")
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
 
     if os.getenv("HBNB_TYPE_STORAGE") != "db":
         @property
