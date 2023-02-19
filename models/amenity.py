2ed174e40cf0691639c2c87b4b20c83183baea0a
Abissa <a.sani@alustudent.com>
2023-02-19 19:44:43 +0200

    fix amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 import os
 
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Float
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
