47bde92dd31d6d48adb39ae5ccd2edb8d3eafad8
Abissa <a.sani@alustudent.com>
2023-03-09 14:12:02 +0200

    2-do_deploy_web_static.py ag

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -3,68 +3,74 @@
 from fabric.api import *
 import os
 import re
+from datetime import datetime
 
 env.user = 'ubuntu'
 env.hosts = ['3.80.74.138', '3.88.68.105']
 
 
+def do_pack():
+    """Comm"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
+
+
 def do_deploy(archive_path):
     """Comment"""
-
-    # Check if the archive file exists
     if not os.path.isfile(archive_path):
         return False
-    filename_regex = re.compile(r'[^/]+\.tgz$')
+
+    filename_regex = re.compile(r'[^/]+(?=\.tgz$)')
     match = filename_regex.search(archive_path)
+
     # Upload the archive to the /tmp/ directory of the web server
     archive_filename = match.group(0)
-    result = put(archive_path, "/tmp/{}".format(archive_filename))
+    result = put(archive_path, "/tmp/{}.tgz".format(archive_filename))
     if result.failed:
         return False
     # Uncompress the archive to the folder
     #     /data/web_static/releases/<archive filename without extension> on
     #     the web server
 
-    # get the release directory
-    release_directory = "/data/web_static/releases/{}".format(
-        archive_filename.replace(".tgz", ""))
-
-    # create the release directory
-    result = run("mkdir -p {}".format(release_directory))
+    result = run(
+        "mkdir -p /data/web_static/releases/{}/".format(archive_filename))
+    if result.failed:
+        return False
+    result = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
+                 .format(archive_filename, archive_filename))
     if result.failed:
         return False
 
-    # uncompress the data
-    result = run("tar -xzf /tmp/{} -C {}"
-                 .format(archive_filename, release_directory))
-    if result.failed:
-        return False
     # Delete the archive from the web server
-    result = run("rm /tmp/{}".format(archive_filename))
+    result = run("rm /tmp/{}.tgz".format(archive_filename))
     if result.failed:
         return False
     result = run("mv /data/web_static/releases/{}"
                  "/web_static/* /data/web_static/releases/{}/"
-                 .format(release_directory,
-                         release_directory))
+                 .format(archive_filename, archive_filename))
     if result.failed:
         return False
-    result = run("rm -rf /data/web_static/releases/{}/web_static".format(
-        release_directory))
+    result = run("rm -rf /data/web_static/releases/{}/web_static"
+                 .format(archive_filename))
     if result.failed:
         return False
-    # Update the symbolic link
-    current_link = "/data/web_static/current"
 
     # Delete the symbolic link /data/web_static/current from the web server
-    result = run("rm -f {}".format(current_link))
+    result = run("rm -rf /data/web_static/current")
     if result.failed:
         return False
+
     #  Create a new the symbolic link
     #  /data/web_static/current on the web server,
     #     linked to the new version of your code
     #     (/data/web_static/releases/<archive filename without extension>)
-    result = run("ln -sf {} {}".format(release_directory, current_link))
+    result = run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
+                 .format(archive_filename))
     if result.failed:
         return False
 
