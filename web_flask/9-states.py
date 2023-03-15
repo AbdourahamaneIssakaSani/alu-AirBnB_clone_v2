e790b8bbeb1e38640f3ce0e7c1a45e3fd38aae07
Abissa <a.sani@alustudent.com>
2023-03-15 15:14:27 +0200

    10. States and State

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,2 +1,44 @@
 #!/usr/bin/python3
 
+"""Starts a Flask web application"""
+
+from models import storage
+from models.state import State
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def state_list():
+    """Comment"""
+    states = storage.all('State').values()
+    return render_template(
+        "9-states.html",
+        states=states,
+        condition="states_list")
+
+
+@app.route('/states/<id>', strict_slashes=False)
+def states__by_id(id):
+    """Comment"""
+    all_states = storage.all('State')
+    try:
+        state_id = all_states[id]
+        return render_template(
+            '9-states.html',
+            state_id=state_id,
+            condition="state_id")
+    except KeyError:
+        return render_template('9-states.html', condition="not_found")
+
+
+@app.teardown_appcontext
+def teardown(self):
+    """Removes the current SQLAlchemy Session"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
