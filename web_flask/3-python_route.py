dfea53534e1b4124886b539a5b7db62446a1826a
Abissa <a.sani@alustudent.com>
2023-03-15 11:58:56 +0200

    3 to 6

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -19,14 +19,14 @@ def hbnb():
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def text_var(text):
+def text_route(text):
     """Comment"""
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def text_var_python(text="is cool"):
+def text_route_python(text="is cool"):
     """Comment"""
     return "Python {}".format(text.replace("_", " "))
 
