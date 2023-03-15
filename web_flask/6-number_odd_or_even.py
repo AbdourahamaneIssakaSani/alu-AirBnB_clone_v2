3bb58346f2e3654e601347a55f7cdbbaea12e27f
Abissa <a.sani@alustudent.com>
2023-03-15 12:20:58 +0200

    fix real 6

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -46,7 +46,7 @@ def num_route_template(n):
 @app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def num_route_even_odd(n):
     """Comment"""
-    return render_template("6-number_odd_or_even.html", n=n)
+    return render_template("6-number_odd_or_even.html", num=n)
 
 
 if __name__ == '__main__':
